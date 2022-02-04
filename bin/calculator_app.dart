d4a293b6320466cd933704ea28aeb3180dc0f569
deepak-mehta01 <dmehta6699@gmail.com>
2022-02-04 17:07:33 +0530

    Addition method implemented

--- bin/calculator_app.dart
+++ bin/calculator_app.dart
@@ -1,3 +1,20 @@
+import 'dart:io';
+
 void main(List<String> arguments) {
-  print('Hello world!');
+  print('Please Enter Two Numbers');
+
+  int val1 = int.parse(stdin.readLineSync()!);
+  int val2 = int.parse(stdin.readLineSync()!);
+  print('Here are your two numbers \n $val1, $val2');
+
+  var calc = Calculation();
+  calc.addition(val1, val2);
+}
+
+class Calculation {
+  void addition(int num1, int num2) {
+    int result = (num1 + num2);
+
+    print('The Sum is \n $result');
+  }
 }
